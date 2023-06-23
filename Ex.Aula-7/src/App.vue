fcdd0af5331f3824fa58a18198aef9e17668e800
Hiansdt <vinijao123690@gmail.com>
2023-06-23 10:47:49 -0300

    Botão com Slot

---
+++ Ex.Aula-7/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <div class="all">
+
+    
+
+  </div>
+
+</template>
---
+++ Ex.Aula-7/src/components/meuBotao.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+    import { ref } from 'vue'
+
+    const corBotao = ref('')
+    const corBotaoHover = ref('')
+
+</script>
+
+<template>
+
+    <button></button>
+
+</template>
